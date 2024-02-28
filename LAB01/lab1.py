6cea56aca73c7d2c1f34a3a72f94d1bc4684bb95
Jun <venusmomo2017@gmail.com>
2024-02-27 20:57:17 -0500

    lab
    lab

--- LAB01/lab1.py
+++ LAB01/lab1.py
@@ -1,82 +1,109 @@
-#
-#   These are the unit tests for functions and classes of lab 1
-#   To use this, run: python test_lab1.py
-#   Tested using Python 3.10
-#
+# function name: wins_rock_scissors_paper
+# inputs: string player, string opponent
+# returns a boolean value as per rock paper scissors hand game rules: https://wrpsa.com/the-official-rules-of-rock-paper-scissors/ (read : What are the rules of RPS?)
+# .lower() : To compare alternating caps
+def wins_rock_scissors_paper(player,opponent):
+    if(player.lower() == "rock" and opponent.lower() == "scissors"):
+        return True
+    elif(player.lower() == "paper" and opponent.lower() == "rock"):
+        return True
+    elif(player.lower() == "scissors" and opponent.lower() == "paper"):
+        return True
+    elif(player.lower() == opponent.lower()):
+        return False
+    else:
+        return False
+	
 
-import unittest
-# from lab1 import wins_rock_scissors_paper, factorial, fibonacci, sum_to_goal,UpCounter,DownCounter
-from lab1another import wins_rock_scissors_paper, factorial, fibonacci, sum_to_goal,UpCounter,DownCounter
+# function name: factorial
+# inputs: integer int_n
+# returns int_n! if int_n > 0. If int_n == 0, returns1
+def factorial(int_n):
+    int_x = 1
+    const_y = int_n
+    fact = const_y
 
-class Lab1TestCase(unittest.TestCase):
-    """These are the test cases for functions and classes of lab1"""
+    if(int_n == 0):
+        return 1
+
+    while(int_x < int_n):
+        fact = fact * (const_y - int_x)
+        int_x += 1
+    return fact
+
+
+# function name: fibonacci
+# input: a non-negative integer n
+# returns nth fibonacci number
+def fibonacci(n):
+    seq_01 = [0,1] #seq_01 list contains fibonacci_0 and fibonacci_1
+
+    if(n == 0):
+        return seq_01[0]
+
+    if(n == 1):
+        return seq_01[1]
+
+    # the nth fibonacci number is the sum of the 2 previous fibonacci numbers.
+    # below, before while loop, pre_digit and pre_pre_digit i.e. previous 2 digits have been assigned for fibonacci_3
+    pre_digit = seq_01[1] #1
+    pre_pre_digit = seq_01[0] #0
+    start = 3
+    result = pre_digit + pre_pre_digit; #fibonacci_2 = 1
+
+    if(n == 2):
+        return result
+
+    pre_pre_digit = pre_digit #1
+    pre_digit = result #1
     
-    def test_win_rock_scissors_paper(self):
-        self.assertEqual(wins_rock_scissors_paper("rock","scissors"),True)
-        self.assertEqual(wins_rock_scissors_paper("rock","paper"),False)
-        self.assertEqual(wins_rock_scissors_paper("scissors".upper(),"paper"),True)
-        self.assertEqual(wins_rock_scissors_paper("Scissors","Rock"),False)
-        self.assertEqual(wins_rock_scissors_paper("paper","sCiSsOrs"),False)
-        self.assertEqual(wins_rock_scissors_paper("paper".title(),"ROCK"),True)
-        self.assertEqual(wins_rock_scissors_paper("paper","PaPeR"),False)
-        self.assertEqual(wins_rock_scissors_paper("rock","ROCK"),False)
-        self.assertEqual(wins_rock_scissors_paper("SCISSORS","scissors"),False)
+    while(start <= n): #loop begins at 3
+        result = pre_digit + pre_pre_digit;
+        pre_pre_digit = pre_digit
+        pre_digit = result
+        start += 1
+    return result
 
 
-    def test_factorial(self):
-        self.assertEqual(factorial(0),1)
-        self.assertEqual(factorial(1),1)
-        self.assertEqual(factorial(19),121645100408832000)
-        self.assertEqual(factorial(8),40320)
+
+#function name: sum_to_goal
+#inputs: number_list which is list of numbers, goal_val which is a number
+#This function finds the two numbers in the list that sum up to the goal value. Function returns the product of the two numbers(the product is the result of multiplying the two numbers together) . If there are no two numbers that when summed results in the goal state, function returns 0
+def sum_to_goal(number_list,goal_val):
+    list_len = len(number_list)
+
+    for x in range(0,list_len):
+        for y in range(0,list_len):
+            sum_2 = (number_list[x] + number_list[y])
+            if(sum_2 == goal_val):
+                result = number_list[x] * number_list[y]
+                return result
+            else:
+                continue
+    
+    return 0
+	
+
+# class : UpCounter
+class UpCounter:
+    counter = 0 #counter=0 for every object created
+
+    # If no step_size mentioned, then self.step_size = 1
+    def __init__(self):
+        self.step_size = 1
+
+    def __init__(self,step_size):
+        self.step_size = step_size
+    
+    def count(self):
+        return self.counter
+    
+    def update(self):
+        self.counter += self.step_size
+        
 
 
-    def test_fibonacci(self):
-        self.assertEqual(fibonacci(0),0)
-        self.assertEqual(fibonacci(1),1)
-        self.assertEqual(fibonacci(2),1)
-        self.assertEqual(fibonacci(3),2)
-        self.assertEqual(fibonacci(35),9227465)
-
-
-    def test_sum_to_goal(self):
-        mylist =[5741, 5742, 4234, 1950, 2255, 3899, 974, 1332, 726, 4208, 2914, 4721, 2094, 2252, 1892, 
-                676, 3097, 2725, 1639, 1122, 4212, 3191, 616, 5346, 1121, 444, 2873, 2597, 1134, 1262, 3838, 
-                1564, 4176, 1873, 4068, 3277, 1765, 4431, 1256, 924, 3440, 4143, 5444, 5653, 5436, 3992, 4902, 
-                2476, 5976, 3699, 2683, 2786, 4001, 2293, 2191, 2530, 4336, 3000, 4713, 2061, 4900, 2844, 128, 
-                4539, 465, 550, 5067, 2636, 5579, 512, 323, 4547, 4125, 4112, 4746, 3860, 1104, 1261, 1791, 5301, 
-                3293, 1464, 3989, 193, 4036, 1132, 3247, 4618, 4033, 3332, 3579, 3221, 5410, 2242, 1495, 2513, 
-                4430, 4508, 3262, 3259]
-
-        self.assertEqual(sum_to_goal(mylist,8716),18969664)
-        self.assertEqual(sum_to_goal(mylist,3385),1470976)
-        self.assertEqual(sum_to_goal(mylist,7327),13257612)
-        self.assertEqual(sum_to_goal(mylist,3103),2399496)
-        self.assertEqual(sum_to_goal(mylist,3470),632461)
-        self.assertEqual(sum_to_goal(mylist,0),0)
-        self.assertEqual(sum_to_goal(mylist,3471),0)
-        self.assertEqual(sum_to_goal(mylist,5080),0)
-
-
-    def test_UPCounter(self):
-        counter=UpCounter(5)
-        counter.update()
-        counter.update()
-        counter.update()
-        self.assertEqual(counter.count(),15)
-        counter.update()
-        counter.update()
-        self.assertEqual(counter.count(),25)
-
-
-    def test_DownCounter(self):
-        counter=DownCounter(3)
-        counter.update()
-        counter.update()
-        counter.update()
-        self.assertEqual(counter.count(),-9)
-        counter.update()
-        counter.update()
-        self.assertEqual(counter.count(),-15)
- 
-if __name__ == '__main__':
-    unittest.main()
+# class : DownCounter
+class DownCounter(UpCounter):
+    def update(self):
+        self.counter -= self.step_size
